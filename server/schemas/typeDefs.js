3078a653e512f4fb53ce6422c88c9b589b5d179a
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-09-20 12:00:24 -0500

    typeDefs,user model added

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,18 +1,33 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
     type User {
         _id: ID
         userName: String
+        firstName: String
+        lastName: String
+        email: String
+        # There is now a field to store the user's password
+        password: String
     }
     
+  # Set up an Auth type to handle returning data from a user creating or user login
+   type Auth {
+   token: ID!
+   user: User
+  }
+
     type Query {
         users: [User]!
         user(userId: ID!): User
     }
     
     type Mutation {
-        addUser(userName: String!): User
+        #Set up mutations to handle creating a user or logging into a user profile and return Auth type
+          addUser(userName: String!, firstName: String!, lastName: String!, email: String, password: String!): Auth
+            login(email: String!, password: String!): Auth 
+
+         removeUser(userId: ID!): User   
     }
     `;
 
