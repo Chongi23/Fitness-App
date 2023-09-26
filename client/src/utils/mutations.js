e43a03057003ffe8e5d4e1bf8900a78f55278686
De La Josh <joshr1979@gmail.com>
2023-09-26 13:28:46 -0500

    deleted second mutation keyword from line 5 mutations js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
 mutation addUser($userName: String!, $firstName: String!, $lastName: String!, $email: String!, $password: String!) {
-    mutation addUser(userName: $userName, firstName: $firstName,lastName: $lastName,email: $email,password: $password) {
+    addUser(userName: $userName, firstName: $firstName,lastName: $lastName,email: $email,password: $password) {
         token
         user {
             _id
