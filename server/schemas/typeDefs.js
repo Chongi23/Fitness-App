9407b925f4fb9a3c94d5f739db946ecd6a197f94
Krysta Gonzalez <gnosisknows28@gmail.com>
2023-09-27 11:49:30 -0500

    edited models/ typedefs, notes included for changes

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,8 @@ const typeDefs = gql`
         firstName: String!
         lastName: String!
         email: String!
-        workouts: [String]!
+        # Changed from "workouts: [String] to workouts: [Workout], do we need exclamation point for [Workout]?
+        workouts: [Workout]
 
         # There is now a field to store the user's password
         password: String
@@ -24,7 +25,27 @@ const typeDefs = gql`
         users: [User]!
         user(userId: ID!): User
     }
-    
+ # Added workout type def - KLG 09/27/23
+
+    type Workout {
+      _id: ID 
+      title: String!
+      description: String
+      exercises: [Exercise]
+      }
+
+      type Exercise {
+        _id:ID
+        name: String
+        sets: Int
+        reps: Int
+        weight: Int
+        notes: String
+      }
+
+    ### Create a new exercise for a specific workout.  This will be used in mutations and queries
+  
+
     type Mutation {
         #Set up mutations to handle creating a user or logging into a user profile and return Auth type
           addUser(userName: String!, firstName: String!, lastName: String!, email: String!, password: String!): Auth
